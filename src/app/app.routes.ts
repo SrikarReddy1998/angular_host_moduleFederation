1edaa13c335485e89295f2e139d037c9c52adb8d
srikarreddy1998 <master.srikarreddy@gmail.com>
2024-10-01 05:39:51 +0530

    Sharing Component from Remote Repo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'child-repo-component',
+    loadComponent() {
+      return loadRemoteModule({
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        remoteName: 'angularChildModuleFederation',
+        exposedModule: './child',
+      })
+        .then((m) => m.AppComponent)
+        .catch((err) => console.log(err, 'component'));
+    },
+  },
+  //   {
+  //     path: 'child-module',
+  //     loadChildren: () => {
+  //       return loadRemoteModule({
+  //         remoteEntry: 'http://localhost:4201/remoteEntry.js',
+  //         remoteName: 'angularChild',
+  //         exposedModule: './angularChild',
+  //       })
+  //         .then((m) => m.ChildMModule)
+  //         .catch((err) => console.log(err));
+  //     },
+  //   },
+];
