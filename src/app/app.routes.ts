fd82ef40ea58c968dc1f6a0df5621ad87d2147c1
srikarreddy1998 <master.srikarreddy@gmail.com>
2024-10-07 23:26:59 +0530

    Connecting react vite native federation component with angular module federation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { loadRemoteModule } from '@angular-architects/module-federation';
+import {
+  WebComponentWrapper,
+  WebComponentWrapperOptions,
+} from '@angular-architects/module-federation-tools';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
@@ -25,5 +29,42 @@ export const routes: Routes = [
         .then((m) => m.ChildMModule)
         .catch((err) => console.log(err));
     },
+  },
+  {
+    path: 'angularNative',
+    loadComponent: () =>
+      loadRemoteModule('angularNative', './Component').then(
+        (m) => m.AppComponent
+      ),
+  },
+  //   {
+  //     path: 'angularNativeChild',
+  //     loadComponent: () =>
+  //       loadRemoteModule('angularNativeChild', './Component').then(
+  //         (m) => m.AppComponent
+  //       ),
+  //   },
+  {
+    path: 'angularNativeChild',
+    loadComponent() {
+      return loadRemoteModule({
+        remoteEntry: 'http://localhost:4223/remoteEntry.json',
+        remoteName: 'angularNativeChild',
+        exposedModule: './Component',
+      })
+        .then((m) => m.AppComponent)
+        .catch((err) => console.log(err, 'component'));
+    },
+  },
+  {
+    path: 'react',
+    component: WebComponentWrapper,
+    data: {
+      // type: 'module',
+      remoteEntry: 'http://localhost:4173/assets/remoteEntry.js',
+      remoteName: 'components-app',
+      exposedModule: './ExposingButton',
+      elementName: 'react-element',
+    } as WebComponentWrapperOptions,
   },
 ];
